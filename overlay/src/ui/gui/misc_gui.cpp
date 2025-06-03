64bb5e9ac8e173d837118492cee376a909bde479
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2025-06-03 07:17:43 -0700

    Update misc_gui.cpp - rename config to settings

--- overlay/src/ui/gui/misc_gui.cpp
+++ overlay/src/ui/gui/misc_gui.cpp
@@ -164,7 +164,7 @@ void MiscGui::updateConfigToggles() {
 
 void MiscGui::listUI()
 {
-    this->listElement->addItem(new tsl::elm::CategoryHeader("Config"));
+    this->listElement->addItem(new tsl::elm::CategoryHeader("Settings"));
 
     this->enabledToggle = new tsl::elm::ToggleListItem("Enable", false);
     enabledToggle->setStateChangedListener([this](bool state) {
