637bc4944e9ffdbe1f8be00b5eeca9834976fbc8
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2025-08-12 15:56:11 -0700

    reset function

---
+++ src/ui/gui/base_gui.h
@@ -0,0 +1,38 @@
+/*
+ * --------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <p-sam@d3vs.net>, <natinusala@gmail.com>, <m4x@m4xw.net>
+ * wrote this file. As long as you retain this notice you can do whatever you
+ * want with this stuff. If you meet any of us some day, and you think this
+ * stuff is worth it, you can buy us a beer in return.  - The sys-clk authors
+ * --------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include <tesla.hpp>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include "../style.h"
+#include "../../ipc.h"
+
+class BaseGui : public tsl::Gui
+{
+    public:
+        BaseGui() {}
+        ~BaseGui() {}
+         virtual void preDraw(tsl::gfx::Renderer* renderer);
+        void update() override;
+        tsl::elm::Element* createUI() override;
+        virtual tsl::elm::Element* baseUI() = 0;
+        virtual void refresh() {}
+    private:
+        bool isUsingEOS;
+};
+
+
+extern std::string getVersionString();
+extern bool usingEOS();
---
+++ src/ui/gui/freq_choice_gui.cpp
@@ -0,0 +1,64 @@
+/*
+ * --------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <p-sam@d3vs.net>, <natinusala@gmail.com>, <m4x@m4xw.net>
+ * wrote this file. As long as you retain this notice you can do whatever you
+ * want with this stuff. If you meet any of us some day, and you think this
+ * stuff is worth it, you can buy us a beer in return.  - The sys-clk authors
+ * --------------------------------------------------------------------------
+ */
+
+#include "freq_choice_gui.h"
+
+#include "../format.h"
+#include "fatal_gui.h"
+
+FreqChoiceGui::FreqChoiceGui(std::uint32_t selectedHz, std::uint32_t* hzList, std::uint32_t hzCount, SysClkModule module, FreqChoiceListener listener)
+{
+    this->selectedHz = selectedHz;
+    this->hzList = hzList;
+    this->hzCount = hzCount;
+    this->module = module;  // Add this
+    this->listener = listener;
+}
+
+tsl::elm::ListItem* FreqChoiceGui::createFreqListItem(std::uint32_t hz, bool selected)
+{
+    tsl::elm::ListItem* listItem = new tsl::elm::ListItem(formatListFreqHz(hz), "", true);
+    listItem->setValue(selected ? "\uE14B" : "");
+
+    listItem->setClickListener([this, hz](u64 keys) {
+        if((keys & HidNpadButton_A) == HidNpadButton_A && this->listener)
+        {
+            if(this->listener(hz))
+            {
+                tsl::goBack();
+            }
+            return true;
+        }
+
+        return false;
+    });
+
+    return listItem;
+}
+
+void FreqChoiceGui::listUI()
+{
+    // Add CategoryHeader based on module
+    std::string moduleName = sysclkFormatModule(this->module, true);
+    this->listElement->addItem(new tsl::elm::CategoryHeader(moduleName));
+    
+    this->listElement->addItem(this->createFreqListItem(0, this->selectedHz == 0));
+    std::uint32_t hz;
+    for(std::uint32_t i = 0; i < this->hzCount; i++) {
+        hz = this->hzList[i];
+        // Skip 204 MHz exactly
+        if(moduleName == "Memory" && hz == 204000000) {
+            continue;
+        }
+        
+        this->listElement->addItem(this->createFreqListItem(hz, (hz / 1000000) == (this->selectedHz / 1000000)));
+    }
+    this->listElement->jumpToItem("", "");
+}
