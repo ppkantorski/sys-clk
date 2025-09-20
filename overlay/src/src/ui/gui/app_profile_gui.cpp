cf0806e46ddfeafd2340a7f2107184d5469ff2f3
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2025-09-20 16:52:50 -0700

    update for latest eos and other fixes

---
+++ overlay/src/src/ui/gui/app_profile_gui.cpp
@@ -0,0 +1,127 @@
+/*
+ * --------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <p-sam@d3vs.net>, <natinusala@gmail.com>, <m4x@m4xw.net>
+ * wrote this file. As long as you retain this notice you can do whatever you
+ * want with this stuff. If you meet any of us some day, and you think this
+ * stuff is worth it, you can buy us a beer in return.  - The sys-clk authors
+ * --------------------------------------------------------------------------
+ */
+
+#include "app_profile_gui.h"
+
+#include "../format.h"
+#include "fatal_gui.h"
+
+AppProfileGui::AppProfileGui(std::uint64_t applicationId, SysClkTitleProfileList* profileList)
+{
+    this->applicationId = applicationId;
+    this->profileList = profileList;
+}
+
+AppProfileGui::~AppProfileGui()
+{
+    delete this->profileList;
+}
+
+void AppProfileGui::openFreqChoiceGui(tsl::elm::ListItem* listItem, SysClkProfile profile, SysClkModule module)
+{
+    std::uint32_t hzList[SYSCLK_FREQ_LIST_MAX];
+    std::uint32_t hzCount;
+    Result rc = sysclkIpcGetFreqList(module, &hzList[0], SYSCLK_FREQ_LIST_MAX, &hzCount);
+    if(R_FAILED(rc))
+    {
+        FatalGui::openWithResultCode("sysclkIpcGetFreqList", rc);
+        return;
+    }
+
+    tsl::changeTo<FreqChoiceGui>(this->profileList->mhzMap[profile][module] * 1000000, hzList, hzCount, module, [this, listItem, profile, module](std::uint32_t hz) {
+        this->profileList->mhzMap[profile][module] = hz / 1000000;
+        listItem->setValue(formatListFreqMHz(this->profileList->mhzMap[profile][module]));
+        Result rc = sysclkIpcSetProfiles(this->applicationId, this->profileList);
+        if(R_FAILED(rc))
+        {
+            FatalGui::openWithResultCode("sysclkIpcSetProfiles", rc);
+            return false;
+        }
+
+        return true;
+    });
+}
+
+void AppProfileGui::addModuleListItem(SysClkProfile profile, SysClkModule module)
+{
+    tsl::elm::ListItem* listItem = new tsl::elm::ListItem(sysclkFormatModule(module, true));
+    listItem->setValue(formatListFreqMHz(this->profileList->mhzMap[profile][module]));
+    listItem->setClickListener([this, listItem, profile, module](u64 keys) {
+        if((keys & HidNpadButton_A) == HidNpadButton_A)
+        {
+            this->openFreqChoiceGui(listItem, profile, module);
+            return true;
+        }
+        else if((keys & HidNpadButton_Y) == HidNpadButton_Y)
+        {
+            // Reset to "Do not override" (0 MHz)
+            this->profileList->mhzMap[profile][module] = 0;
+            listItem->setValue(formatListFreqMHz(0));
+            
+            // Save the updated profile
+            Result rc = sysclkIpcSetProfiles(this->applicationId, this->profileList);
+            if(R_FAILED(rc))
+            {
+                FatalGui::openWithResultCode("sysclkIpcSetProfiles", rc);
+                return false;
+            }
+            return true;
+        }
+        return false;
+    });
+    this->listElement->addItem(listItem);
+}
+
+void AppProfileGui::addProfileUI(SysClkProfile profile)
+{
+    this->listElement->addItem(new tsl::elm::CategoryHeader(sysclkFormatProfile(profile, true) + std::string(" ") + ult::DIVIDER_SYMBOL + "  Reset"));
+    this->addModuleListItem(profile, SysClkModule_CPU);
+    this->addModuleListItem(profile, SysClkModule_GPU);
+    this->addModuleListItem(profile, SysClkModule_MEM);
+}
+
+void AppProfileGui::listUI()
+{
+    this->addProfileUI(SysClkProfile_Docked);
+    this->addProfileUI(SysClkProfile_Handheld);
+    this->addProfileUI(SysClkProfile_HandheldCharging);
+    this->addProfileUI(SysClkProfile_HandheldChargingOfficial);
+    this->addProfileUI(SysClkProfile_HandheldChargingUSB);
+}
+
+void AppProfileGui::changeTo(std::uint64_t applicationId)
+{
+    SysClkTitleProfileList* profileList = new SysClkTitleProfileList;
+    Result rc = sysclkIpcGetProfiles(applicationId, profileList);
+    if(R_FAILED(rc))
+    {
+        delete profileList;
+        FatalGui::openWithResultCode("sysclkIpcGetProfiles", rc);
+        return;
+    }
+
+    tsl::changeTo<AppProfileGui>(applicationId, profileList);
+}
+
+void AppProfileGui::update()
+{
+    BaseMenuGui::update();
+
+    if((this->context && this->applicationId != this->context->applicationId) &&  this->applicationId != SYSCLK_GLOBAL_PROFILE_TID)
+    {
+        tsl::changeTo<FatalGui>(
+            "Application changed\n\n"
+            "\n"
+            "The running application changed\n\n"
+            "while editing was going on.",
+            ""
+        );
+    }
+}
---
+++ overlay/src/src/ui/gui/global_override_gui.cpp
@@ -0,0 +1,108 @@
+/*
+ * --------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <p-sam@d3vs.net>, <natinusala@gmail.com>, <m4x@m4xw.net>
+ * wrote this file. As long as you retain this notice you can do whatever you
+ * want with this stuff. If you meet any of us some day, and you think this
+ * stuff is worth it, you can buy us a beer in return.  - The sys-clk authors
+ * --------------------------------------------------------------------------
+ */
+
+#include "global_override_gui.h"
+
+#include "fatal_gui.h"
+#include "../format.h"
+
+GlobalOverrideGui::GlobalOverrideGui()
+{
+    for(std::uint16_t m = 0; m < SysClkModule_EnumMax; m++)
+    {
+        this->listItems[m] = nullptr;
+        this->listHz[m] = 0;
+    }
+}
+
+void GlobalOverrideGui::openFreqChoiceGui(SysClkModule module)
+{
+    std::uint32_t hzList[SYSCLK_FREQ_LIST_MAX];
+    std::uint32_t hzCount;
+    Result rc = sysclkIpcGetFreqList(module, &hzList[0], SYSCLK_FREQ_LIST_MAX, &hzCount);
+    if(R_FAILED(rc))
+    {
+        FatalGui::openWithResultCode("sysclkIpcGetFreqList", rc);
+        return;
+    }
+    tsl::changeTo<FreqChoiceGui>(this->context->overrideFreqs[module], hzList, hzCount, module, [this, module](std::uint32_t hz) {
+        Result rc = sysclkIpcSetOverride(module, hz);
+        if(R_FAILED(rc))
+        {
+            FatalGui::openWithResultCode("sysclkIpcSetOverride", rc);
+            return false;
+        }
+
+        this->lastContextUpdate = armGetSystemTick();
+        this->context->overrideFreqs[module] = hz;
+
+        return true;
+    });
+}
+
+void GlobalOverrideGui::addModuleListItem(SysClkModule module)
+{
+    tsl::elm::ListItem* listItem = new tsl::elm::ListItem(sysclkFormatModule(module, true));
+    listItem->setValue(formatListFreqMHz(0));
+    listItem->setClickListener([this, module](u64 keys) {
+        if((keys & HidNpadButton_A) == HidNpadButton_A)
+        {
+            this->openFreqChoiceGui(module);
+            return true;
+        }
+        else if((keys & HidNpadButton_Y) == HidNpadButton_Y)
+        {
+            // Reset override to "Do not override" (0 Hz)
+            Result rc = sysclkIpcSetOverride(module, 0);
+            if(R_FAILED(rc))
+            {
+                FatalGui::openWithResultCode("sysclkIpcSetOverride", rc);
+                return false;
+            }
+            
+            // Update context and tracking variables
+            this->lastContextUpdate = armGetSystemTick();
+            this->context->overrideFreqs[module] = 0;
+            this->listHz[module] = 0;
+            
+            // Update display
+            this->listItems[module]->setValue(formatListFreqHz(0));
+            
+            return true;
+        }
+        return false;
+    });
+    this->listElement->addItem(listItem);
+    this->listItems[module] = listItem;
+}
+
+void GlobalOverrideGui::listUI()
+{
+    this->listElement->addItem(new tsl::elm::CategoryHeader("Temporary Overrides " + ult::DIVIDER_SYMBOL + "  Reset"));
+    this->addModuleListItem(SysClkModule_CPU);
+    this->addModuleListItem(SysClkModule_GPU);
+    this->addModuleListItem(SysClkModule_MEM);
+}
+
+void GlobalOverrideGui::refresh()
+{
+    BaseMenuGui::refresh();
+    if(this->context)
+    {
+        for(std::uint16_t m = 0; m < SysClkModule_EnumMax; m++)
+        {
+            if(this->listItems[m] != nullptr && this->listHz[m] != this->context->overrideFreqs[m])
+            {
+                this->listItems[m]->setValue(formatListFreqHz(this->context->overrideFreqs[m]));
+                this->listHz[m] = this->context->overrideFreqs[m];
+            }
+        }
+    }
+}
