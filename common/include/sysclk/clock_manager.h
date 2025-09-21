3e19e2e29541d1fbbab4f66cebd6e0df9fd4dc16
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2025-09-20 21:54:53 -0700

    Update clock_manager.h

--- common/include/sysclk/clock_manager.h
+++ common/include/sysclk/clock_manager.h
@@ -24,6 +24,7 @@ typedef struct
     uint32_t temps[SysClkThermalSensor_EnumMax];
     int32_t power[SysClkPowerSensor_EnumMax];
     uint32_t ramLoad[SysClkRamLoad_EnumMax];
+    uint32_t realVolts[4];
 } SysClkContext;
 
 typedef struct
